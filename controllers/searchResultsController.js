9f796805e370c051da371b903e47462b83cc8dc8
Melanie De Giusti <meludegiusti@Melanies-MacBook-Pro.local>
2021-04-25 16:01:56 -0300

    Intento buscador

--- controllers/searchResultsController.js
+++ controllers/searchResultsController.js
@@ -2,11 +2,21 @@ let autos = require('../data/autos');
 
 let controller = {
     product: function(req, res) {
-        res.render("searchResults", {"infoAuto": autos});
+        res.render("searchResults", {"autosid": autos});
     },
-    
-    
-    
+    nombre: function(req, res) {
+        let nombres = req.params.nombre;
+        let resultados=[];
+        for (let i = 0; i < autos.length; i++){
+            if(autos[i].nombre == nombres){
+                resultados.push(autos[i])
+            }
+        };
+        console.log(resultados);
+        res.render("searchResults", {'autosid': resultados})
+
+    },
 };
+
 
 module.exports = controller;
--- routes/searchResults.js
+++ routes/searchResults.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 let searchResultsController = require("../controllers/searchResultsController");
 
+ 
+
 router.get('/', searchResultsController.product); 
-
-
+router.get('/:id', searchResultsController.nombre); 
 
 
 module.exports = router;
